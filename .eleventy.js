5e3b6aa5bfcfbe76be51b41c340864bd8e3c167c
John Doe <johndoe@example.com>
2026-01-24 12:27:29 +0100

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,48 +1,42 @@
-const util = require('node:util')
-const exec = util.promisify(require('node:child_process').exec)
+const util = require("node:util")
+const exec = util.promisify(require("node:child_process").exec)
 
 module.exports = function (config) {
+	const { readdir, stat } = require("fs/promises")
 
-	const { readdir, stat } = require('fs/promises')
-
-
-	config.addWatchTarget('./src/assets/css/')
-	config.addWatchTarget('./src/assets/scripts/')
-	config.addWatchTarget('./src/*.js')
+	config.addWatchTarget("./src/assets/css/")
+	config.addWatchTarget("./src/assets/scripts/")
+	config.addWatchTarget("./src/*.js")
 
 	config.setWatchJavaScriptDependencies(true)
 
 	config.setUseGitIgnore(false)
 
-	config.addPassthroughCopy('./src/*.css')
-	config.addPassthroughCopy('./src/*.js')
+	config.addPassthroughCopy("./src/*.css")
+	config.addPassthroughCopy("./src/*.js")
 
-	config.addPassthroughCopy('./src/*/*.png')
-	config.addPassthroughCopy('./src/*/*.jpg')
-	config.addPassthroughCopy('./src/*/*.jpeg')
-	config.addPassthroughCopy('./src/*/*.webp')
-	config.addPassthroughCopy('./src/old_2022/')
-	config.addPassthroughCopy('./src/*/*.webm')
-
-
+	config.addPassthroughCopy("./src/*/*.png")
+	config.addPassthroughCopy("./src/*/*.jpg")
+	config.addPassthroughCopy("./src/*/*.jpeg")
+	config.addPassthroughCopy("./src/*/*.webp")
+	config.addPassthroughCopy("./src/old_2022/")
+	config.addPassthroughCopy("./src/*/*.webm")
 
 	config.addCollection("mergedCollection", async function (collections) {
-
 		function onlyImages(file) {
 			const isImage = new RegExp(/\.(png|jpg|jpeg|gif|webp|webm)$/g)
 			if (isImage.test(file.name)) {
 				return true
+			} else {
+				return false
 			}
-			else { return false }
 		}
-
 
 		function isVideo(file) {
 			const isVideo = new RegExp(/\.(mp4|webm)$/g)
 			if (isVideo.test(file.name)) {
 				return true
-			}
-			else {
+			} else {
 				return false
 			}
 		}
@@ -54,34 +48,37 @@ module.exports = function (config) {
 				const { stdout } = await exec(command)
 				return {
 					name,
-					time: Number(stdout)
+					time: Number(stdout),
 				}
 			} catch (e) {
 				if (e instanceof Error) {
-					console.log(new Error(`Failed executing ${command} with ${e.message}`))
+					console.log(
+						new Error(`Failed executing ${command} with ${e.message}`),
+					)
 				}
 				const metadata = await stat(`${basePath}/${name}`)
 				return {
 					name,
-					time: metadata.mtime.getTime()
+					time: metadata.mtime.getTime(),
 				}
 			}
 		}
 
 		try {
-			const basePath = 'src/images/'
+			const basePath = "src/images/"
 			const dir = await readdir(basePath, { withFileTypes: true })
 
-			const posts = collections.getFilteredByTag('post')
+			const posts = collections.getFilteredByTag("post")
 
 			const sortedSnaps = (
-				await Promise.all(dir
-					.map(fileName => gettingCommitedDate(basePath, fileName))
-				)).filter(onlyImages)
-				.map(file => {
-
+				await Promise.all(
+					dir.map((fileName) => gettingCommitedDate(basePath, fileName)),
+				)
+			)
+				.filter(onlyImages)
+				.map((file) => {
 					let data = {
-						date: new Date(file.time * 1000)
+						date: new Date(file.time * 1000),
 					}
 					if (isVideo(file)) {
 						data.name = file.name
@@ -104,19 +101,18 @@ module.exports = function (config) {
 		}
 	})
 
-	config.addFilter('formatDate', function (date) {
-		return date.toLocaleDateString('FR-FR')
+	config.addFilter("formatDate", function (date) {
+		return date.toLocaleDateString("FR-FR")
 	})
 
 	return {
 		dir: {
-			input: 'src',
-			output: 'public_html',
+			input: "src",
+			output: "public_html/morphogeneses",
 		},
 		passthroughFileCopy: true,
-		templateFormats: ['html', 'njk', 'md'],
-		htmlTemplateEngine: 'njk',
-		markdownTemplateEngine: 'njk'
-
+		templateFormats: ["html", "njk", "md"],
+		htmlTemplateEngine: "njk",
+		markdownTemplateEngine: "njk",
 	}
 }
