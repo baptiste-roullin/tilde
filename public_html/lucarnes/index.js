6c3fcd29d2abf457372691237886bc9740a10cfb
John Doe <johndoe@example.com>
2026-01-25 22:39:56 +0100

    deploy

--- public_html/lucarnes/index.js
+++ public_html/lucarnes/index.js
@@ -338,4 +338,18 @@ document.addEventListener("DOMContentLoaded", async (e) => {
 })
 
 
+const dialog = document.querySelector("dialog")
+const showButton = document.querySelector("header span")
+const closeButton = document.querySelector("dialog button")
+
+// Le bouton "Afficher la fenêtre" ouvre le dialogue
+showButton.addEventListener("click", () => {
+	dialog.showModal()
+})
+
+// Le bouton "Fermer" ferme le dialogue
+closeButton.addEventListener("click", () => {
+	dialog.close()
+})
+
 export { }
